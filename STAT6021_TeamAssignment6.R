c75692e2ae64ddcacc9f1137b139ef020c7ab1ff
saiprasanthblk <lbs7aa@virginia.edu>
2017-10-25 12:45:07 -0400

    Update STAT6021_TeamAssignment6.R

--- STAT6021_TeamAssignment6.R
+++ STAT6021_TeamAssignment6.R
@@ -41,7 +41,127 @@
 #
 #   (a) The linear model using a transformed explanatory variable outperforms the model using the
 #       quadratric term of the explanatory variable.
+
+
+# transformed model
+
+set.seed(1)
+x <- rnorm(30, 20, 10)
+slope <- 3
+intercept <- 0
+error <- rnorm(30, 0, 0.5)
+y <- intercept + slope*log(x) + error
+
+plot(x,y)
+
+model1 <- lm(y~ log(x))
+
+summary(model1)
+
+# R square is 0.99
+
+qqnorm(rstudent(model1))
+qqline(rstudent(model1))
+
+ti<-rstudent(model1)
+yhat <- fitted(model1)
+plot(yhat,ti)
+
+# Scatter plot shows log relationship. R square is very high. Residuals plots look great
+
+model2 <- lm(y~ x + x^2)
+
+summary(model2)
+
+# R square is 0.56
+
+qqnorm(rstudent(model2))
+qqline(rstudent(model2))
+
+ti<-rstudent(model2)
+yhat <- fitted(model2)
+plot(yhat,ti)
+
+# R square is quite low. 
+
 #   (b) The linear model using the quadratric term of the explanatory variable outperforms the model 
 #       using a transformed explanatory variable.
+
+set.seed(1)
+x <- rnorm(30, 20, 10)
+slope <- 3
+intercept <- 0
+error <- rnorm(30, 0, 0.5)
+y <- intercept + x + x^2 + error
+
+plot(x,y)
+
+model1 <- lm(y~ x + x^2)
+
+summary(model1)
+
+# R square is 0.90
+
+qqnorm(rstudent(model1))
+qqline(rstudent(model1))
+
+ti<-rstudent(model1)
+yhat <- fitted(model1)
+plot(yhat,ti)
+
+# The errors show an ideal quadratic pattern
+
+model2 <- lm(y~ log(x))
+
+summary(model2)
+
+# R square is just 0.34
+
+qqnorm(rstudent(model2))
+qqline(rstudent(model2))
+
+ti<-rstudent(model2)
+yhat <- fitted(model2)
+plot(yhat,ti)
+
+# clearly the quadratic model is better
+
 #   (c) The linear model using a transformed explanatory variable and the model using the quadratric 
 #       term of the explanatory variable perform equally well.
+
+set.seed(1)
+x <- rnorm(30, 15, 2)
+slope <- 3
+intercept <- 0
+error <- rnorm(30, 0, 0.5)
+y <- intercept + log(x) + x^2 + error
+
+plot(x,y)
+
+model1 <- lm(y~ log(x))
+
+summary(model1)
+
+qqnorm(rstudent(model1))
+qqline(rstudent(model1))
+
+ti<-rstudent(model1)
+yhat <- fitted(model1)
+plot(yhat,ti)
+
+# R square is 0.98
+
+model2 <- lm(y~ x^2)
+
+summary(model2)
+
+# R square is 0.99
+
+qqnorm(rstudent(model2))
+qqline(rstudent(model2))
+
+ti<-rstudent(model2)
+yhat <- fitted(model2)
+plot(yhat,ti)
+
+# even the residuals plots look very similar
